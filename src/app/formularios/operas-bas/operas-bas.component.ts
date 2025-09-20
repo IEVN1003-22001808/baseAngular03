d5f429ac18324cbceea0c24ac456677ce043420e
IEVN1003-22001808 <georgeizac.skul@gmail.com>
2025-09-19 23:54:37 -0600

    Operaciones Básicas 19/09/2025

--- src/app/formularios/operas-bas/operas-bas.component.ts
+++ src/app/formularios/operas-bas/operas-bas.component.ts
@@ -10,18 +10,26 @@ export class OperasBasComponent {
   num1:string='';
   num2:string='';
   resultado:number=0;
+  op: string = '';
 
-  sumar():void{
-    this.resultado=parseInt(this.num1)+parseInt(this.num2);
-  }
-  restar():void{
-    this.resultado=parseInt(this.num1)-parseInt(this.num2);
-  }
-  mult():void{
-    this.resultado=parseInt(this.num1)*parseInt(this.num2);
-  }
-  div():void{
-    this.resultado=parseInt(this.num1)/parseInt(this.num2);
+  calcular(){
+    const n1 = parseInt(this.num1);
+    const n2 = parseInt(this.num2);
+
+    switch (this.op){
+      case 'sumar':
+        this.resultado = n1 + n2;
+      break;
+      case 'restar':
+        this.resultado = n1 - n2;
+      break;
+      case "mult":
+        this.resultado = n1 * n2;
+      break;
+      case 'div':
+        this.resultado = n1 / n2;
+      break;
+    }
   }
 
 }
